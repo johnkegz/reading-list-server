d064649d9190332e7ba1dcb735edfbdb8f15094f
johnkegz <johnkal24@gmail.com>
2020-01-30 15:25:14 +0300

    connect to mongoose database

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
 
 const app = express();
+
+//Connect to database
+mongoose.connect("mongodb+srv://kegz:BXhY5SPMtXe8Qt2b@cluster0-whx3l.mongodb.net/readers-list?retryWrites=true&w=majority")
+mongoose.connection.once('open', ()=>{
+    console.log("connected to data base +>>>>>")
+})
 
 app.use("/graphql", graphqlHTTP({
     schema,
